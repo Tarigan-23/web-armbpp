b544cc286516787f771a586872336fa1d15e3960
Yegar Girsang <yegargirsang@gmail.com>
2026-09-25 00:43:14 +0800

    refactor: gunakan env variables untuk supabase

--- apps/web/src/lib/supabase.js
+++ apps/web/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://zlbiezqiicgtcejdbdpm.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsYmllenFpaWNndGNlamRiZHBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwNzgyMzMsImV4cCI6MjEwNTY1NDIzM30.ZQgG4N5dqs0aoQV0m7Nk9_mwgdA6qc4Uo9-g5eprA0Y';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
